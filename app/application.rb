fb79670b8d4bdb2c6436f619e353701b5c4419c1
theprivatepark <anneypark@gmail.com>
2020-09-13 14:35:15 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,9 @@ class Application
 
   def call(env)
     return [200 , {'Content-Type' => 'text/html'}, pretty_response]
+    #HTTP status code
+    #hash declaring content type
+    #entire content of the webpage(HASH or ARRAY)
   end
 
   def pretty_response
