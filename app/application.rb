d7013511d0a74860c8f44d6cb3a8ac3f3143d895
theprivatepark <anneypark@gmail.com>
2020-09-13 14:33:44 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+require 'rack'
+
+
+class Application
+
+  def call(env)
+    return [200 , {'Content-Type' => 'text/html'}, pretty_response]
+  end
+
+  def pretty_response
+    (Time.now.hour < 12) ? ["Good Morning!"] : ["Good Afternoon!"]
+
+  end
+
+end
+
+
